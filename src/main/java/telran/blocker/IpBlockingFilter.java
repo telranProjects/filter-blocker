b442a0b23d81ed6f3e9b965dff00b3fdcf3ea00a
AlexKovalchuk <kovalchuk111@gmail.com>
2024-04-16 15:34:16 +0300

    updated with list blocked ips

--- src/main/java/telran/blocker/IpBlockingFilter.java
+++ src/main/java/telran/blocker/IpBlockingFilter.java
@@ -1,14 +1,14 @@
 package telran.blocker;
 
 import java.io.IOException;
+import java.util.*;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.sun.tools.sjavac.Log;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -19,12 +19,21 @@ import lombok.extern.slf4j.Slf4j;
 @Component("ipBlockingFilter")
 @Slf4j
 public class IpBlockingFilter extends OncePerRequestFilter {
-	
+
+	private static final long TIME_UPDATED = 600000; // 10 min for example; this is list update period
+
 	RestTemplate restTemplate = new RestTemplate();
-	
-	String url = "/ip/exist";
+
+	String url = "/ip/get_ips"; // ???
 	String host = "localhost";
 	int port = 8484;
+
+	List<String> ipBlockList;
+
+	@Scheduled(fixedRate = TIME_UPDATED)
+	public void periodacallyCallingRemoteProvider() {
+		getUpdateIpBlockList();
+	}
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -32,14 +41,16 @@ public class IpBlockingFilter extends OncePerRequestFilter {
 
 		String ipClient = getClientIp(request);
 
-		if (ipShouldBlocked(ipClient)) {
+		if (ipBlockList.contains(ipClient)) {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN, "access denied");
-			return;
+
+		} else {
+
+			// continue application's filter chain
+
+			filterChain.doFilter(request, response);
 		}
 
-		// continue application's filter chain
-
-		filterChain.doFilter(request, response);
 	}
 
 	private String getClientIp(HttpServletRequest request) {
@@ -50,34 +61,35 @@ public class IpBlockingFilter extends OncePerRequestFilter {
 		return ip;
 	}
 
-	private boolean ipShouldBlocked(String ip) {
-		boolean res = false;
+	@SuppressWarnings("unchecked")
+	private List<String> getUpdateIpBlockList() {
+		ipBlockList.clear();
 		try {
-			ResponseEntity<?> responseEntity = restTemplate.exchange(getFullUrl(ip), HttpMethod.GET, null,
-					Boolean.class);
+			ResponseEntity<?> responseEntity = restTemplate.exchange(getFullUrl(), HttpMethod.GET, null, List.class);
 			if (!responseEntity.getStatusCode().is2xxSuccessful()) {
 				throw new Exception((String) responseEntity.getBody());
 			}
-			res = (boolean) responseEntity.getBody();
-			log.debug("ip {} contain in blocking list: {}", ip, res);
+			ipBlockList = (List<String>) responseEntity.getBody();
+			log.debug("updated block list is {} ", ipBlockList);
 		} catch (Exception e) {
 			log.error("Blocking Data Provider's server is unavailable, {}", e.getMessage());
 		}
-		return res;
+
+		return ipBlockList;
 	}
 
-	private String getFullUrl(String ip) {
-		String res = String.format("hppt://%s:%d%s/%s", host, port, url, ip);
-		return res;
+	private String getFullUrl() {
+		return String.format("http://%s:%d%s", host, port, url);
 	}
-	
-//	@GetMapping("${app.blocking.data.exist.url:/ip/exist}" + "/{ip}")
-//	boolean existsById (@PathVariable(name="ip") String ip) {
+
+//This is supposed end-point method for BlockingDataprovider controller:
+
+//	@GetMapping("${app.blocking.data.list.url:/ip/get_ips}")
+//	List<String> getBlockingList (String ip) {
 //		log.debug("received IP: {}", ip);
-//		boolean res = providerService.existsById(ip);
-//		log.debug("IP: {} is exists: {}", ip, res);
+//		List<String> res = providerService.getBlockingList();
+//		log.trace("received blocking ip list: {}", res);
 //		return res;
 //	}
-
 
 }
